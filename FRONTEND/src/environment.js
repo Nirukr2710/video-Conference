50e5f02fb29538e86f2f4151c47c3f0857a2c3d1
niranjan kumar <nirukr2005@gmail.com>
2026-07-25 23:26:03 +0530

    environment updated

--- FRONTEND/src/environment.js
+++ FRONTEND/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = false;
 const server = IS_PROD ?
-    " https://videoconferencebackend-qco9.onrender.com" :
+    " https://videoconferencebackend-73gt.onrender.com" :
 
     "http://localhost:2020"
 
